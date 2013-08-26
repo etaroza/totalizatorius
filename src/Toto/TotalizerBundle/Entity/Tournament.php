121a083ca2a7df423f6998612284f61f3dc5da6d
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2013-08-26 18:01:13 +0300

    Tournament entity.

---
+++ src/Toto/TotalizerBundle/Entity/Tournament.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Toto\TotalizerBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tournament
+ *
+ * @ORM\Table(name="tournament")
+ * @ORM\Entity
+ */
+class Tournament
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="active", type="integer")
+     */
+    private $active = 1;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Tournament
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set active
+     *
+     * @param integer $active
+     * @return Tournament
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+    
+        return $this;
+    }
+
+    /**
+     * Get active
+     *
+     * @return integer 
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+}
---
+++ src/Toto/TotalizerBundle/TotoTotalizerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Toto\TotalizerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TotoTotalizerBundle extends Bundle
+{
+}
