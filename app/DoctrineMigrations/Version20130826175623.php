5fc5ee68757667ccfa227e487f9b32c15c5944d1
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2013-09-03 09:42:43 +0300

    Fixed migration.

--- app/DoctrineMigrations/Version20130826175623.php
+++ app/DoctrineMigrations/Version20130826175623.php
@@ -15,7 +15,7 @@ class Version20130826175623 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
-        $this->addSql("CREATE TABLE tournament (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, active TINYINT(1) NOT NULL PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB");
+        $this->addSql("CREATE TABLE tournament (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, active TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB");
     }
 
     public function down(Schema $schema)
